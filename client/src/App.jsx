2bd53ac0a69b1d62adb4e52429ad92271eab1b04
Harshit Kumar Tiwari <harshitkt20@gmail.com>
2024-05-24 17:18:35 +0530

    Added the required pages using react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+
 
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-red-800">
-      Hello world!
-    </h1>
+  return (<BrowserRouter>
+  <Routes>
+    <Route path='/' element= {<Home />} />
+    <Route path='/About' element = {<About />} />
+    <Route path='/Prfile' element = {<Profile />} />
+    <Route path='/Signin' element = {<Signin />} />
+    <Route path='/Signup' element = {<Signup />} />
+  </Routes>
+  </BrowserRouter>
   )
 }
